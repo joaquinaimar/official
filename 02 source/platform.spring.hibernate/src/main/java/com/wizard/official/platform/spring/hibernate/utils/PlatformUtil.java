acfddccfb4d2d7918e59c98061d4904d6473eaa1
lizhi <joaquinaimar@163.com>
2014-05-23 11:13:03 +0800

    增加excel文件下载

--- 02 source/platform.spring.hibernate/src/main/java/com/wizard/official/platform/spring/hibernate/utils/PlatformUtil.java
+++ 02 source/platform.spring.hibernate/src/main/java/com/wizard/official/platform/spring/hibernate/utils/PlatformUtil.java
@@ -1,8 +1,13 @@
 package com.wizard.official.platform.spring.hibernate.utils;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
@@ -26,6 +31,32 @@ public class PlatformUtil {
 
 	public static List<FileItem> getFileItem(final HttpServletRequest request) {
 		return getFileItem(request, 0);
+	}
+
+	public static OutputStream getExcelOutputStream(HttpServletRequest request,
+			HttpServletResponse response, String fileName) throws IOException {
+
+		fileName = getFileName(request, fileName);
+
+		response.reset();
+		response.setContentType("application/vnd.ms-excel;utf-8");
+		response.setHeader("Content-Disposition", "attachment; filename="
+				+ fileName);
+
+		return response.getOutputStream();
+
+	}
+
+	public static String getFileName(HttpServletRequest request, String fileName)
+			throws UnsupportedEncodingException {
+		String userAgent = request.getHeader("User-Agent").toLowerCase();
+
+		if (userAgent.indexOf("firefox") > 0) {
+			fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");
+		} else {
+			fileName = URLEncoder.encode(fileName, "UTF-8");
+		}
+		return fileName;
 	}
 
 }
